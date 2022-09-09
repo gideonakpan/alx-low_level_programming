6964e384f98087301be44ecdc82bdec24107ead1
gideonakpan <gideonakpan98@yahoo.com>
2022-09-08 19:44:42 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+
+*A program that print using the put function
+
+ */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
+
